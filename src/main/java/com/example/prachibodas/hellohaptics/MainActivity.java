5ed16dcbcf535fc797a248d29b529ef0edce242b
prbodas <prbodas@gmail.com>
2016-07-31 18:11:16 -0700

    first

---
+++ src/main/java/com/example/prachibodas/hellohaptics/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.prachibodas.hellohaptics;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import com.senseg.haptics.Grain;
+
+public class MainActivity extends AppCompatActivity {
+
+    // prepare for trouble make it double (not float)
+
+    // To protect the world from devastation
+    // to unite all people within our nation
+    // to denounce the evils of truth and love
+    // to extend our reach to the stars above
+
+    // prachi, bobby
+
+    // team haptics
+
+    // blast off with the speed of light
+    // surrender or be prepared to fight
+
+    // meowth! thats right!
+
+
+    // ^DO NOT DELETE THE LINES ABOVE. CODE WILL BREAK IF DELETED.
+    //                                                  -- Prachi. Bodas.
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //Grain g = new Grain(Grain.GRAIN_AREA_BUMPY);
+        //g.play();
+
+    }
+}
